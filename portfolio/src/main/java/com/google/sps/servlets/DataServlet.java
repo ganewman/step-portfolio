dd10dfb3f79e008eb335a8eb15706659fd6cc3b5
Gabi Newman <glnewman00@gmail.com>
2020-06-05 19:58:31 +0000

    Modify comments to include a name field.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -18,6 +18,7 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
+import com.google.sps.data.Comment;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,13 +29,10 @@ import java.util.ArrayList;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private ArrayList<String> comments;
+  private ArrayList<Comment> comments;
 
   public DataServlet() {
-    comments = new ArrayList<>(3);
-    comments.add("Hello!");
-    comments.add("Nice to meet you!");
-    comments.add("Have a nice day!");
+    comments = new ArrayList<>(5);
   }
 
   @Override
@@ -45,19 +43,21 @@ public class DataServlet extends HttpServlet {
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String userComment = request.getParameter("comment-input");
-    long timestamp = System.currentTimeMillis();
+    String name = request.getParameter("name-input");
+    String content = request.getParameter("comment-input");
+    long time = System.currentTimeMillis();
 
     // Uses a hidden form to get the page the user sent the request from
     // Allows redirect to work correctly even if comments section is present on multiple pages
     String pageToRedirect = request.getParameter("page-name");
     
     Entity commentEntity = new Entity("Comment");
-    commentEntity.setProperty("text", userComment);
-    commentEntity.setProperty("time", timestamp);
-    
+    commentEntity.setProperty("name", name);
+    commentEntity.setProperty("content", content);
+    commentEntity.setProperty("time", time);
+
     // TODO remove once code is able to read from database
-    comments.add(0, userComment);
+    comments.add(0, new Comment(name, content, time));
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
